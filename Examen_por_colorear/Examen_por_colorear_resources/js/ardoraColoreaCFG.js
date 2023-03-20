ca8c777fc91c7b2b91ab9a5e915b2cdde24ddf05
Jonr28 <hjonr28@gmail.com>
2023-03-20 11:48:10 -0600

    Actividad por colorear según leyenda
    Parte B de el examen

---
+++ Examen_por_colorear/Examen_por_colorear_resources/js/ardoraColoreaCFG.js
@@ -0,0 +1,36 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=0; attempts=0; attemptsMax=1;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=1
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk=""; messageTime=""; messageError=""; messageErrorG=""; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="RXhhbWVuX3Bvcl9jb2xvcmVhcg=="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var joinPar=[];
+var c=["#0000A0","#FF0000","#FFFF00","#42C521","#BF2D81","#3DE7E2","#FF8000"];
+var x=["288","101","238","164","332","193","107"];
+var y=["68","187","112","194","187","312","66"];
+var t=["","","","","","",""];
+var cM=["#0000A0","#FF0000","#FFFF00","#42C521","#BF2D81","#3DE7E2","#FF8000"];
+var tM=["2+2","4+2","6+6","3+3","10+10","15+15","6+4"];
+var indexColor=0;
